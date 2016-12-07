1b36ec6ff3d04dce9e3298024b8aaf74a3273214
Henrik Söderberg <henkkasoderberg@hotmail.com>
2016-12-07 13:09:39 +0200

    rooms and user numbers implemented

--- index.js
+++ index.js
@@ -6,8 +6,10 @@ var io = require("socket.io")(http);
 var Dict = require("collections/dict");
 
 var dictionary = new Dict();
-var users = new Dict();
+//var users = new Dict();
 
+var rooms = [];
+var clients = [];
 //var users = {};
 
 app.use(express.static(__dirname + "/public"));
@@ -22,66 +24,81 @@ http.listen(3000, function(){
 
 function generateID(){
   var id = Math.random().toString(36).substr(2,10);
-  return id;
+  if(dictionary.has(id)){
+    return generateID();
+  }else{
+    return id;
+  }
 }
 
-
+/*
 function getRoomClients(data){
   var room = io.sockets.adapter.rooms[data];
   console.log(room.length);
   return room.length;
 }
+*/
 
 io.on("connection", function(socket){
+  //for disconnect array deletion process
+  var socketRoom;
+  var socketNumber;
   console.log("Client has connected to the server");
-  socket.on("disconnect", function(){
-    users.delete(String(socket.userNumber));
-    console.log("Client has disconnected");
+
+  socket.on("disconnect", function(reason){
+    console.log("socket room " + socketRoom + " socket number " + socketNumber);
+    delete rooms[socketRoom][socketNumber];
   });
 
   socket.on("createroom", function(){
     var roomName = generateID();
     socket.room = roomName;
     socket.join(socket.room);
-
     socket.userNumber = 0;
-    users.add(0, "0");
-    //users[socket.userNumber] = socket.userNumber;
+    socketRoom = roomName;
+    socketNumber = 0;
 
     dictionary.add(roomName, roomName);
+    var users = [];
+    users[0] = 0;
+    console.log("USERS 0 " + users[0]);
+    rooms[socket.room] = users;
+    console.log(rooms[socket.room]);
 
     console.log("Room " + roomName + " created");
     console.log("User ID set to " + socket.userNumber);
   });
 
 //recursively finds "first free slot" in the dictinary
-function firstfree(data){
-  //console.log(data);
-  if(users.has(String(data))){
-    return firstfree(data + 1);
+function firstfree(data, roomid){
+  console.log(rooms[roomid]);
+  if(rooms[roomid][data] != null){
+    return firstfree(data + 1, roomid);
   }else{
-    users.add(data, String(data));
+    rooms[roomid][data] = data;
     return data;
   }
 }
 
   socket.on("joinroom", function(data){
-    if(dictionary.has(data.roomid) && users.length < 5){
+    if(dictionary.has(data.roomid)){
       socket.room = data.roomid;
-      var numb = firstfree(0);
+      var numb = firstfree(0, data.roomid);
       socket.userNumber = numb;
+      socketRoom = data.roomid;
+      socketNumber = numb;
 
       socket.emit("roomjoined");
       console.log("Client connected to room " + data.roomid);
       console.log("Clients ID is " + socket.userNumber);
     }else{
-      var data;
-      if(users.length == 5){
-        data = "Room full"
+      var msg;
+      if(rooms[data.roomid].length == 5){
+        msg = "Room full"
       }else{
-        data = "No such ID"
+        msg = "No such ID"
       }
-      socket.emit("nojoin", {message: data});
+      socket.emit("nojoin", {message: msg});
     }
   });
 });
